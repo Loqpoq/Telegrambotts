d428de2eebcd5aa8752188ffbda875ae09b616e2
Tenzin Phuljung <tenzinphuljung7@gmail.com>
2016-02-02 16:15:48 -0600

    Add animation canvas and functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,70 +1,21 @@
-$(document).ready() {
-  $('.tlt').textillate({
-  // the default selector to use when detecting multiple texts to animate
-  selector: '.texts',
+window.onload = function () {
+  var myName = "Tenzin.";
+  var letterColors = [blue, orange, red, white];
+  bubbleShape = "square";
+  
+  var canvas = document.getElementById('myCanvas');
+  canvas.width  = 150;
+  canvas.height = 150;
+  canvas.style.width  = '550px';
+  canvas.style.height = '200px';
+  canvas.style.padding = "0px";
+  canvas.style.position = "absolute";
 
-  // enable looping
-  loop: true,
 
-  // sets the minimum display time for each text before it is replaced
-  minDisplayTime: 2000,
+  document.getElementsByClassName('tlt')[0].appendChild(canvas);;
 
-  // sets the initial delay before starting the animation
-  // (note that depending on the in effect you may need to manually apply
-  // visibility: hidden to the element before running this plugin)
-  initialDelay: 0,
+  drawName(myName, letterColors);
 
-  // set whether or not to automatically start animating
-  autoStart: true,
+  bounceName();
 
-  // custom set of 'in' effects. This effects whether or not the
-  // character is shown/hidden before or after an animation
-  inEffects: [],
-
-  // custom set of 'out' effects
-  outEffects: [],
-
-  // in animation settings
-  in: {
-    // set the effect name
-    effect: 'bounceInDown',
-
-    // set the delay factor applied to each consecutive character
-    delayScale: 1.5,
-
-    // set the delay between each character
-    delay: 50,
-
-    // set to true to animate all the characters at the same time
-    sync: true,
-
-    // randomize the character sequence
-    // (note that shuffle doesn't make sense with sync = true)
-    shuffle: false,
-
-    // reverse the character sequence
-    // (note that reverse doesn't make sense with sync = true)
-    reverse: false,
-
-    // callback that executes once the animation has finished
-    callback: function () {}
-  },
-
-  // out animation settings.
-  out: {
-    effect: 'hinge',
-    delayScale: 1.5,
-    delay: 50,
-    sync: false,
-    shuffle: false,
-    reverse: false,
-    callback: function () {}
-  },
-
-  // callback that executes once textillate has finished
-  callback: function () {},
-
-  // set the type of token to animate (available types: 'char' and 'word')
-  type: 'char'
-});
 }
